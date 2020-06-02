19666478a32ac6a5e4295b471ddc1ef2c06d1659
marinahaid <60192957+marinahaid@users.noreply.github.com>
2020-06-02 14:27:24 +0300

    canvas

---
+++ js/stat.js
@@ -0,0 +1,46 @@
+'use strict';
+
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGHT = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+var FONT_GAP = 16;
+var TEXT_WIDTH = 40;
+var BAR_HEIGHT = 150;
+var BAR_INDENT = 50;
+var barheight = CLOUD_HEIGHT - GAP - FONT_GAP - GAP - FONT_GAP - GAP- FONT_GAP - GAP - GAP - FONT_GAP -GAP;
+//var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var BAR_WIDTH = 40;
+
+
+var renderCloud = function(ctx, x, y, color) {
+ctx.fillStyle = color;
+ctx.fillRect = (x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i + + ) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+  return maxElement;
+}
+
+window.renderStatistics = function(ctx, players, times) {
+renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+renderCloud(ctx, CLOUD_X , CLOUD_Y, '#fff');
+
+
+ctx.fillStyle = '#000';
+
+var maxTime = getMaxElement(times);
+
+for (var i = 0; i < player.lenght; i ++) {
+ctx.fillText(player[i], CLOUD_X + GAP, CLOUD_Y +  GAP + BAR_HEIGHT);
+ctx.fillRect(CLOUD_X + GAP + (TEXT_WIDTH + BAR_INDENT) * i, CLOUD_Y + GAP, barWidth + time[i])/maxTime, BAR_HEIGHT);
+}
+}
