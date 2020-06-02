3d0bb3d33eb0eb4db40a3e18fea09768505b77f0
marinahaid <60192957+marinahaid@users.noreply.github.com>
2020-06-03 02:11:17 +0300

    canvas text rect

--- js/stat.js
+++ js/stat.js
@@ -9,38 +9,40 @@ var FONT_GAP = 16;
 var TEXT_WIDTH = 40;
 var BAR_HEIGHT = 150;
 var BAR_INDENT = 50;
-var barheight = CLOUD_HEIGHT - GAP - FONT_GAP - GAP - FONT_GAP - GAP- FONT_GAP - GAP - GAP - FONT_GAP -GAP;
-//var barWidth = CLOUD_WIDTH - GAP - TEXT_WIDTH - GAP;
+var barheight = CLOUD_HEIGHT - GAP - FONT_GAP - GAP - FONT_GAP - GAP - FONT_GAP - GAP;
+
 var BAR_WIDTH = 40;
 
 
-var renderCloud = function(ctx, x, y, color) {
-ctx.fillStyle = color;
-ctx.fillRect = (x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
 
-  for (var i = 0; i < arr.length; i + + ) {
+  for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
   }
   return maxElement;
-}
+};
 
-window.renderStatistics = function(ctx, players, times) {
-renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
-renderCloud(ctx, CLOUD_X , CLOUD_Y, '#fff');
+window.renderStatistics = function (ctx, players, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
 
 
-ctx.fillStyle = '#000';
+  ctx.fillStyle = '#000';
 
-var maxTime = getMaxElement(times);
+  var maxTime = getMaxElement(times);
 
-for (var i = 0; i < player.lenght; i ++) {
-ctx.fillText(player[i], CLOUD_X + GAP, CLOUD_Y +  GAP + BAR_HEIGHT);
-ctx.fillRect(CLOUD_X + GAP + (TEXT_WIDTH + BAR_INDENT) * i, CLOUD_Y + GAP, barWidth + time[i])/maxTime, BAR_HEIGHT);
-}
-}
+  for (var i = 0; i < players.length; i++) {
+    ctx.fillText(players[i], CLOUD_X + GAP + BAR_INDENT + (BAR_WIDTH + BAR_INDENT) * i, CLOUD_Y + GAP + BAR_HEIGHT + (GAP + FONT_GAP) * 3);
+    ctx.fillRect((CLOUD_X + GAP) * i, CLOUD_Y + GAP + BAR_WIDTH + (barheight + times[i]) / maxTime, BAR_HEIGHT);
+
+  }
+};
+
